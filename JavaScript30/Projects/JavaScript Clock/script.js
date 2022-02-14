4cbc85b22a4bd763982db4d0e22b759e5ccd0148
scarlettmiracle <99689113+scarlettmiracle@users.noreply.github.com>
2022-02-14 16:51:06 -0500

    clock2

--- JavaScript30/Projects/JavaScript Clock/script.js
+++ JavaScript30/Projects/JavaScript Clock/script.js
@@ -1,23 +1,20 @@
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+"use strict";
+function setDate()
+{
+    var e=new Date,t=e.getSeconds(),
+        n=t/60*360+90,
+        a=e.getMinutes(),
+        s=a/60*360+90,
+        r=e.getHours(),
+        o=r/12*360+a/60*30+90;
 
-function setDate() {
-    const now = new Date();
-
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-    const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-
-    const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+   secondHand.style.transform="rotate("+n+"deg)",minuteHand.style.transform="rotate("+s+"deg)",
+   hourHand.style.transform="rotate("+o+"deg)",secondHand.style.transition=90===n?"all 0s":"all 0.5s",
+   minuteHand.style.transition=90===s?"all 0s":"all 0.5s"
 }
 
-setInterval(setDate, 1000);
+var secondHand=document.querySelector(".second-hand"),
+    minuteHand=document.querySelector(".min-hand"),
+    hourHand=document.querySelector(".hour-hand");
 
-setDate();
+setInterval(setDate,1e3);
