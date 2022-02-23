830d393158effba085512fd5d1406afcdd55095e
scarlettmiracle <99689113+scarlettmiracle@users.noreply.github.com>
2022-02-23 11:23:34 -0500

    php

---
+++ PHP/Measure/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Measurement Conversion</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="main-content">
+		<h1>Measurement Conversion</h1>
+		<div id="measurement-types">
+			<span><a href="length.php">Length and Distance</a></span>
+			<span><a href="area.php">Area</a></span>
+			<span><a href="volume.php">Volume and Capacity</a></span>
+			<span><a href="mass.php">Mass and Weight</a></span>
+			<span><a href="speed.php">Speed</a></span>
+			<span><a href="temperature.php">Temperature</a></span>
+		</div>
+	</div>
+</body>
+</html>
